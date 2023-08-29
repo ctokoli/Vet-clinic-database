e48d288e425d9e84f7fa4911e9f356dab92c353e
ctokoli <matumasie@gmail.com>
2023-08-29 08:46:22 +0000

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -1,3 +1,7 @@
-/*Queries that provide answers to the questions from all projects.*/
+id/*Queries that provide answers to the questions from all projects.*/
 
 SELECT * from animals WHERE name = 'Luna';
+
+SELECT name FROM animals WHERE name LIKE '%mon';
+
+SELECT name from animals WHERE EXTRACT(YEAR FROM date_of_birth) BETWEEN 2016 AND 2019;
