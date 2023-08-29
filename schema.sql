f5187b8ec1c6aa9e168e2d7491b49a0104c9af72
ctokoli <matumasie@gmail.com>
2023-08-29 08:46:22 +0000

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,5 +1,13 @@
 /* Database schema to keep the structure of entire database. */
 
-CREATE TABLE animals (
-    name varchar(100)
+CREATE TABLE IF NOT EXISTS animals (
+    id INT PRIMARY KEY,
+    name VARCHAR(200),
+    date_of_birth date,
+    escape_attempts INT,
+    neutered BOOLEAN,
+    weight_kg DECIMAL
 );
+
+INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg) 
+VALUES (1, 'Agumon', DATE '2020-02-03', 0, true, 10.23);
