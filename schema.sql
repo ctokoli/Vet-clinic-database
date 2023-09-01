8edb751c1a108734b9d8f283d7d6b4f23d8e6efd
ctokoli <matumasie@gmail.com>
2023-09-01 10:33:11 +0000

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -10,3 +10,25 @@ CREATE TABLE IF NOT EXISTS animals (
 );
 
 ALTER TABLE animals ADD COLUMN species VARCHAR(200);
+
+
+ALTER TABLE animals DROP COLUMN species;
+
+CREATE TABLE IF NOT EXISTS owners (
+    id serial PRIMARY KEY,
+    full_name VARCHAR(200),
+    age INT
+);
+
+CREATE TABLE IF NOT EXISTS species (
+    id serial PRIMARY KEY,
+    name VARCHAR(200)
+);
+
+ALTER TABLE animals ADD COLUMN owner_id INT,
+ADD CONSTRAINT fk_owner
+    FOREIGN KEY (owner_id) REFERENCES owners(id);
+
+ALTER TABLE animals ADD COLUMN species_id INT, 
+ADD CONSTRAINT fk_species
+    FOREIGN KEY (species_id) REFERENCES species(id);
